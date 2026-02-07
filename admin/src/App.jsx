db8a6c9e5979b5febe74255ee63a5340deb57707
Shorya <raghav.shorya123@gmail.com>
2026-02-07 17:16:47 +0530

    Change API URL from 4000 to 5000

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import {ToastContainer,toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
-  const url="http://localhost:4000"
+  const url="http://localhost:5000"
   return (
     <div>
       <ToastContainer/>
